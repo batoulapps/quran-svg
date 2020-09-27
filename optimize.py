c54023ff0a3d1d43ee5c8b06f0e2c3725ca16c0b
Ameir Al-Zoubi <ameir.alzoubi@gmail.com>
2020-09-27 02:32:48 +0000

    Keep ayah numbers in SVG
    using consistent naming convention

--- optimize.py
+++ optimize.py
@@ -1,7 +1,7 @@
 from xml.dom import minidom
 from os import walk, path
 
-def isContentNode(node):
+def is_content_node(node):
     if node.firstChild == None:
         return False
     if node.firstChild.tagName != 'path':
@@ -10,69 +10,72 @@ def isContentNode(node):
         return False
     return True
 
-def removeNodeWithName(nodeName, xml):
-    nodes = xml.getElementsByTagName(nodeName)
+def remove_tags(tag_names, xml):
+    for tag_name in tag_names:
+        elements = xml.getElementsByTagName(tag_name)
+        for element in elements:
+            parent = element.parentNode
+            parent.removeChild(element)
+
+def remove_nodes(nodes):
     for node in nodes:
         parent = node.parentNode
         parent.removeChild(node)
 
-def adjustRootTransform(node, filename):
+def move_node(node, new_parent):
+    moving_node = node.parentNode.removeChild(node)
+    new_parent.appendChild(moving_node)
+
+def adjust_root_transform(node, filename):
     page_number = int(filename.replace('.svg', ''))
     horizontal_offset = '-115' if page_number % 2 == 0 else '-55'
     node.setAttribute('transform', f'matrix(1.3333333,0,0,-1.3333333,{horizontal_offset},640)')
 
-def setViewBox(node, width, height):
+def set_viewbox(node, width, height):
     node.setAttribute('width', f'{width}')
     node.setAttribute('height', f'{height}')
     node.setAttribute('viewBox', f'0 0 {width} {height}')
 
-def optimizeOpeningPage(xml, filename):
-    setViewBox(xml.firstChild, 235, 235)
+def optimize_opening_page(xml, filename):
+    set_viewbox(xml.firstChild, 235, 235)
     
-    main_node = xml.firstChild.firstChild
-    main_node.setAttribute('transform', f'matrix(1.3333333,0,0,-1.3333333,-136,482)')
+    root_group = xml.firstChild.firstChild
+    root_group.setAttribute('transform', f'matrix(1.3333333,0,0,-1.3333333,-136,482)')
     
-    top_groups = main_node.childNodes
-
-    content_group = top_groups[len(top_groups) - 1]
-    ayah_markers = top_groups[len(top_groups) - 2] # todo parse ayah markers to record ayah end positions
-    to_remove = top_groups[0:len(top_groups) - 1]
-
-    for node in to_remove:
-        parent = node.parentNode
-        parent.removeChild(node)
+    root_children = root_group.childNodes
     
-    group_nodes = content_group.getElementsByTagName('g')
-    translated_group = [x for x in group_nodes if x.hasAttribute('transform')][-1]
-    node = translated_group.parentNode.removeChild(translated_group)
-    main_node.appendChild(node)
-
-    content_group.parentNode.removeChild(content_group)
-
-def optimizeStandardPage(xml, filename):
-    setViewBox(xml.firstChild, 345, 550)
+    border_group = root_children[0]
+    ayah_marker_group = root_children[1] # todo parse ayah markers to record ayah end positions
+    content_group = root_children[2]
     
-    main_node = xml.firstChild.firstChild
-    adjustRootTransform(main_node, filename)
-
-    top_groups = main_node.childNodes
-
-    content_group = top_groups[len(top_groups) - 1]
-    ayah_markers = top_groups[len(top_groups) - 2] # todo parse ayah markers to record ayah end positions
-    to_remove = top_groups[0:len(top_groups) - 1]
-
-    for node in to_remove:
-        parent = node.parentNode
-        parent.removeChild(node)
+    remove_nodes([border_group])
     
-    group_nodes = content_group.getElementsByTagName('g')
-    translated_groups = [x for x in group_nodes if isContentNode(x)]
-    
-    for translated_group in translated_groups:
-        node = translated_group.parentNode.removeChild(translated_group)
-        main_node.appendChild(node)
+    content_sub_groups = content_group.getElementsByTagName('g')
+    content_node = [x for x in content_sub_groups if is_content_node(x)][-1]
+    move_node(content_node, root_group)
+    remove_nodes([content_group])
 
-    content_group.parentNode.removeChild(content_group)
+def optimize_standard_page(xml, filename):
+    set_viewbox(xml.firstChild, 345, 550)
+    
+    root_group = xml.firstChild.firstChild
+    adjust_root_transform(root_group, filename)
+
+    root_children = root_group.childNodes
+    
+    content_group = root_children[len(root_children) - 1]
+    ayah_markers = root_children[2] # todo parse ayah markers to record ayah end positions
+    to_remove = [root_children[0], root_children[1]] + root_children[3:len(root_children) - 1]
+    
+    remove_nodes(to_remove)
+    
+    content_sub_groups = content_group.getElementsByTagName('g')
+    content_nodes = [x for x in content_sub_groups if is_content_node(x)]
+    
+    for content_node in content_nodes:
+        move_node(content_node, root_group)
+
+    remove_nodes([content_group])
 
 def main():
     files = list()
@@ -88,13 +91,12 @@ def main():
         filepath = path.join(svg_dir, filename)
         xml = minidom.parse(filepath)
 
-        removeNodeWithName('metadata', xml)
-        removeNodeWithName('defs', xml)
+        remove_tags(['metadata', 'defs'], xml)
         
         if filename in ['001.svg', '002.svg']:
-            optimizeOpeningPage(xml, filename)
+            optimize_opening_page(xml, filename)
         else:
-            optimizeStandardPage(xml, filename)
+            optimize_standard_page(xml, filename)
 
         output = xml.toxml()
         xml.unlink()
